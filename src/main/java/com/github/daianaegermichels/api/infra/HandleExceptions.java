5cbed4f285ab077dcd8d6ed373f16d6e31de714e
Daiana Michels <daianaem.12@gmail.com>
2023-02-22 21:26:39 -0300

    implements method for error 400

--- src/main/java/com/github/daianaegermichels/api/infra/HandleExceptions.java
+++ src/main/java/com/github/daianaegermichels/api/infra/HandleExceptions.java
@@ -2,6 +2,8 @@ package com.github.daianaegermichels.api.infra;
 
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -12,4 +14,17 @@ public class HandleExceptions {
     public ResponseEntity entityNotFound() {
         return ResponseEntity.notFound().build();
     }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity methodArgumentNotValid(MethodArgumentNotValidException ex){
+        var error = ex.getFieldErrors();
+        return ResponseEntity.badRequest().body(error.stream().map(ValidationErrorData::new).toList());
+    }
+
+    private record ValidationErrorData (String field, String message) {
+        public ValidationErrorData (FieldError error) {
+            this(error.getField(), error.getDefaultMessage());
+        }
+    }
+
 }
